22b1c54b55f62a3e73f34578739b241e6ee5c2b1
Sander Latour <latour@uva.nl>
2013-02-25 22:47:02 +0100

    Added LOG_OUTPUT flag that determines if log messages are shown, this can be set with the function setouput [0|1]

--- log.sh
+++ log.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+LOG_OUTPUT=1
+
 # Usage: log "LABEL" "MESG" ["color"]
 function log {
-  if [ $# -lt 2 ]; then
-    log "ERROR" "Usage: log \"LABEL\" \"MESG\" [\"FGCOLOR\"]" "red"
-  elif [ $# -eq 3 ]; then
-    _FG="$(fgcolor)"
-    fgcolor "$3"
-    echo "[$1 | `date +%R`] $2"
-    fgcolor "${_FG}"
+  if [ "$LOG_OUTPUT" -eq 1 ]; then
+    if [ $# -lt 2 ]; then
+      log "ERROR" "Usage: log \"LABEL\" \"MESG\" [\"FGCOLOR\"]" "red"
+    elif [ $# -eq 3 ]; then
+      _FG="$(fgcolor)"
+      fgcolor "$3"
+      echo "[$1 | `date +%R`] $2"
+      fgcolor "${_FG}"
+    else
+      echo "[$1 | `date +%R`] $2"
+    fi
+  fi
+}
+
+function setoutput {
+  if [ $# -gt 0 -a "$1" == "0" ]; then
+    LOG_OUTPUT=0
   else
-    echo "[$1 | `date +%R`] $2"
+    LOG_OUTPUT=1
   fi
 }
